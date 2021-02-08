0ef6156b71b8fa03dc4d6fe888e43f5bdf21e288
JeongYunMi <tamd5971@gmail.com>
2021-02-08 21:11:18 +0900

    login 디자인 틀

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from 'containers/home/Home';
+import Login from 'containers/login/Login';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route path="/login" component={Login} />
   </Switch>
 );
 
