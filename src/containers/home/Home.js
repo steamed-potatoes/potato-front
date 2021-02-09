68b65ba43067c28c9c1f2cf8ac8edc88509964b6
JeongYunMi <tamd5971@gmail.com>
2021-02-09 17:56:02 +0900

    eslint 설정

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <a href="/Login">Sign In now</a>
+    </div>
+  );
 }
