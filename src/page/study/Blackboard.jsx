0f86d0d52f70a9e27aac3553d95136aefb1d7c3a
minho0315 <dlalsgh369@naver.com>
2021-02-23 16:58:37 +0900

    use styled-components

--- src/page/study/Blackboard.jsx
+++ src/page/study/Blackboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
-import "./style.css"
+import styled from 'styled-components';
+// import "./style.css"
 
 const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
@@ -13,14 +13,58 @@ const useInput = (initialValue) => {
 const Blackboard = () => {
   const name = useInput('');
   return (
-    <div>
-      <h1 className="h1tag">디지털 칠판</h1>
-      <div>
-        <input className="outputtag" value={name.value} />
-        <input className="inputtag" placeholder="입력해주세요" {...name}  />
-      </div>
-    </div>
+    <Wrapper>
+      <Ptag className="h1tag">디지털 칠판</Ptag>
+      <Outputtag className="outputtag" value={name.value} />
+      <Inputtag className="inputtag" placeholder="입력해주세요" {...name}  />
+    </Wrapper>
   );
 };
 
 export default Blackboard;
+
+const Wrapper = styled.div `
+ display: flex;
+ flex-direction: column;
+ -webkit-box-pack: center;
+ justify-content: center;
+ -webkit-box-align: center;
+ align-items: center;
+`;
+
+const Ptag = styled.p`
+  margin-top: 80px;
+  margin-left: auto;
+  margin-right: auto;
+  height: 160px;
+  font-size: 80px;
+  text-align: left;
+  color: rgb(0, 0, 0);
+  margin: 0px;
+  white-space: pre-wrap;
+  font-weight: normal;
+`;
+
+const Inputtag = styled.input`
+  outline: none;
+  border: 1px solid rgb(0, 0, 0);
+  width: 1024px;
+  padding: 8px;
+  font-size: 16px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Outputtag = styled.input`
+  margin-top: 80px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid rgb(0, 0, 0);
+  background-color: rgb(30, 130, 76);
+  color: rgb(255, 255, 255);
+  text-align: center;
+  font-size : 24px;
+  width: 1024px;
+  height: 400px;
+`;
