4ce06dcf2e6bc8a53a8b1c9c3da205ba5da86317
bigyou98 <bigyou00@gmail.com>
2021-02-26 12:16:46 +0900

    add comit

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,11 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+
 const Home = lazy(() => import('./page/home/Home'));
 const Login = lazy(() => import('./page/login/Login'));
 const GoogleCallback = lazy(() => import('./page/google/GoogleCallback'));
+const Blackboard = lazy(() => import('./page/study/Blackboard'));
 
 const Root = () => {
   return (
@@ -12,6 +14,7 @@ const Root = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/Login" component={Login} />
+          <Route path="/Blackboard" component={Blackboard} />
           <Route path="/auth/google/callback" component={GoogleCallback} />
         </Switch>
       </Suspense>
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 const Home = () => {
   return (
     <div>
       Home
       <a href="/Login">Sign In now</a>
+      <a href="/Blackboard">Blackboard</a>
     </div>
   );
 };
