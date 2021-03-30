404c18085ce98a0c9afb35670771e83197b0233d
minho0315 <dlalsgh369@naver.com>
2021-03-31 02:22:20 +0900

    login layout

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -1,28 +1,101 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FcGoogle } from 'react-icons/fc';
+import { AiOutlineArrowRight } from "react-icons/ai";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-top : 24px;
+`;
+
+const Header = styled.div`
+  font-size : 24px;
+  height : 72px;
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Manager = styled.div``;
+
+const MenuList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const MenuItem = styled.li`
+  float : right;
+  display : block;
+  padding: 16px 16px;
+`;
+
+const LoginWrapper = styled.div`
+  margin-top : 160px;
+  display : flex;
+  flex-direction : column;
+  justify-content : center;
+  align-items : center;
+`;
+
+const Title = styled.p`
+  margin : 0px;
+  font-size : 56px;
+`;
+
+const SubTitle = styled.p`
+  margin : 8px 0px 16px 0px;
+  font-size : 16px;
+`;
+
+const LoginButton = styled.button`
+  margin : 24px 0px 0px 0px;
+  padding : 16px 32px 16px 32px;
+  width: 720px;
+  display : flex;
+  flex-direction : row;
+  justify-content: space-between;
+  align-items : center;
+  background-color: white;
+  border: 0px solid white;
+  border-radius: 48px;
+  box-shadow: 0px 24px 3px -16px #cfcece;
+  &:hover {
+    cursor: pointer;
+    border: 1px solid #cfcece;
+  }
+  &:focus { outline:none; }
+`;
+
+const LoginButtonText = styled.p`
+ margin : 16px 24px 16px 24px;
+ display : block;
+ font-size : 24px;
+`;
+
 
 const Login = () => {
 
   return (
     <Wrapper>
-      <div>
-        <p>관리자</p>
-        <ul>
-          <li>Home</li>
-          <li>Login</li>
-        </ul>
-      </div>
-      <div>
-        <p>슬기로운 감자생활</p>
-        <p>이용하시려면 하단의 아이디로 로그인 하기를 클릭 하세오</p>
-        <div>
-          <p>이미지</p>
-          <button type="button">구글 아이디로 시작하기</button>
-          <p>화살표</p>
-        </div>
-      </div>
+      <Header>
+        <Manager>관리자</Manager>
+        <MenuList>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Login</MenuItem>
+        </MenuList>
+      </Header>
+      <LoginWrapper>
+        <Title>슬기로운 감자생활</Title>
+        <SubTitle>
+          이용하시려면 하단의 아이디로 로그인 하기를 클릭 하세오
+        </SubTitle>
+        <LoginButton>
+          <FcGoogle size="88px" />
+          <LoginButtonText>구글 아이디로 시작하기</LoginButtonText>
+          <AiOutlineArrowRight size="40px" />
+        </LoginButton>
+      </LoginWrapper>
     </Wrapper>
   );
 };
