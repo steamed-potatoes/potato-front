4fbc2b573b6f739847ad84de1621483d758dddc7
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-04-05 23:04:34 +0900

    fixed to 동철's advice.

--- src/page/myPage/MyPage.jsx
+++ src/page/myPage/MyPage.jsx
@@ -8,9 +8,10 @@ return (
   <Main>
     <ProfileBox>
       <EditProfile>Edit Profile</EditProfile>
+      <Line />
       <PhotoBox>
         <BsFillCircleFill size="389" color="gray" />
-        <FiCamera type="button" size="50" />
+        <CameraBtn><FiCamera size="80" /></CameraBtn>
       </PhotoBox>
       <TextBox>
         <List>
@@ -28,38 +29,47 @@ return (
   </Main>
   );
 };
+// 배경이미지 왜안되는지 모르겟슴...
 const Main = styled.div`
-  
+  width : 100vw;
+  height : 100vh;
+  background-image: url("src/asset/img/background.png");
 `
 const ProfileBox = styled.div`
   margin: auto;
-  margin-top: 50px;
+  margin-top: 56px;
   display: flex;
   justify-content: space-around;
-  width : 1645px;
-  height : 762px;
+  width : 1648px;
+  height : 760px;
   flex-flow : row wrap;
   box-shadow: -20px 30px 15px gray; 
 `;
 const EditProfile = styled.div`
-  width : 1645px;
-  margin-left: 50px;
-  font-size : 58px;
+  width : 1640px;
+  margin-left: 48px;
+  margin-top : 8px;
+  font-size : 56px;
   align-self: flex-start; 
 `
 const PhotoBox = styled.div`
   position : relative;
+  height : 400px;
 `;
-// const CameraBtn = styled.button`
-//   position : absolute;
-//   left : 520px;
-//   top : 580px;
-//   z-index: 1;
-// `;
+const CameraBtn = styled.button`
+  position : absolute;
+  border : none;
+  right : 24px;
+  bottom : 24px;
+  z-index: 1;
+  cursor : pointer;
+`;
 const TextBox = styled.div`
+  height : 480px;
 `;
 const List = styled.ul`
   list-style:none;
+  width : 880px;
 `;
 const MainName  = styled.span`
   font-size: 58px;
@@ -67,9 +77,15 @@ const MainName  = styled.span`
 `;
 const GroupName  = styled.span`
   font-size: 30px;
-  letter-spacing : 30px;
+  word-spacing : 30px;
 `;
 const ListItem = styled.li`
-  margin-bottom : 15px;
+  margin-bottom : 32px;
+`
+const Line = styled.hr`
+  width : 1640px;
+  height : 0px;
+  border : 1px solid gray;
+  margin-top : -48px;
 `
 export default MyPage;
