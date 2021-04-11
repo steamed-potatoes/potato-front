59d28cc8cbfcddd737f55426fff74c22a0ccd166
ruso99 <hondongk@naver.com>
2021-04-11 18:34:20 +0900

    Feat: Mypage 라인 수정

--- src/page/myPage/MyPage.jsx
+++ src/page/myPage/MyPage.jsx
@@ -1,91 +1,108 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BsFillCircleFill } from "react-icons/bs";
-import { FiCamera } from "react-icons/fi";
+import { BsFillCircleFill } from 'react-icons/bs';
+import { FiCamera } from 'react-icons/fi';
 
 const MyPage = () => {
-return (
-  <Main>
-    <ProfileBox>
-      <EditProfile>Edit Profile</EditProfile>
-      <Line />
-      <PhotoBox>
-        <BsFillCircleFill size="389" color="gray" />
-        <CameraBtn><FiCamera size="80" /></CameraBtn>
-      </PhotoBox>
-      <TextBox>
-        <List>
-          <ListItem><MainName>이서윤</MainName></ListItem>
-          <hr />
-          <ListItem>김이선</ListItem>
-          <ListItem>시간디자인과</ListItem>
-          <ListItem>201710083</ListItem>
-          <ListItem>이메일</ListItem>
-          <hr />
-          <ListItem> <GroupName>디자인 온 </GroupName></ListItem>
-        </List>
-      </TextBox>
-    </ProfileBox>
-  </Main>
+  return (
+    <Main>
+      <ProfileBox>
+        <EditProfile>Edit Profile</EditProfile>
+        <LineBox>
+          <Line width="48" color="gray" />
+          <Line width="100" color="yellow" />
+          <Line width="1492" color="gray" />
+        </LineBox>
+        <PhotoBox>
+          <BsFillCircleFill size="389" color="gray" />
+          <CameraBtn>
+            <FiCamera size="80" />
+          </CameraBtn>
+        </PhotoBox>
+        <TextBox>
+          <List>
+            <ListItem>
+              <MainName>이서윤</MainName>
+            </ListItem>
+            <hr />
+            <ListItem>김이선</ListItem>
+            <ListItem>시간디자인과</ListItem>
+            <ListItem>201710083</ListItem>
+            <ListItem>이메일</ListItem>
+            <hr />
+            <ListItem>
+              {' '}
+              <GroupName>디자인 온 </GroupName>
+            </ListItem>
+          </List>
+        </TextBox>
+      </ProfileBox>
+    </Main>
   );
 };
 
 const Main = styled.div`
-  width : 100vw;
-  height : 100vh;
-  background-image: url("src/asset/img/background.png");
-`
+  width: 100vw;
+  height: 100vh;
+  background-image: url('src/asset/img/background.png');
+`;
 const ProfileBox = styled.div`
   margin: auto;
   margin-top: 56px;
   display: flex;
   justify-content: space-around;
-  width : 1648px;
-  height : 760px;
-  flex-flow : row wrap;
-  box-shadow: -20px 30px 15px gray; 
+  width: 1648px;
+  height: 760px;
+  flex-flow: row wrap;
+  box-shadow: -20px 30px 15px gray;
 `;
 const EditProfile = styled.div`
-  width : 1640px;
+  width: 1640px;
   margin-left: 48px;
-  margin-top : 8px;
-  font-size : 56px;
-  align-self: flex-start; 
-`
+  margin-top: 8px;
+  font-size: 56px;
+  align-self: flex-start;
+`;
 const PhotoBox = styled.div`
-  position : relative;
-  height : 400px;
+  position: relative;
+  height: 400px;
 `;
 const CameraBtn = styled.button`
-  position : absolute;
-  border : none;
-  right : 24px;
-  bottom : 24px;
+  position: absolute;
+  border: none;
+  right: 24px;
+  bottom: 24px;
   z-index: 1;
-  cursor : pointer;
+  cursor: pointer;
 `;
 const TextBox = styled.div`
-  height : 480px;
+  height: 480px;
 `;
 const List = styled.ul`
-  list-style:none;
-  width : 880px;
+  list-style: none;
+  width: 880px;
 `;
-const MainName  = styled.span`
+const MainName = styled.span`
   font-size: 58px;
-  letter-spacing : 30px;
+  letter-spacing: 30px;
 `;
-const GroupName  = styled.span`
+const GroupName = styled.span`
   font-size: 30px;
-  word-spacing : 30px;
+  word-spacing: 30px;
 `;
 const ListItem = styled.li`
-  margin-bottom : 32px;
-`
+  margin-bottom: 32px;
+`;
 const Line = styled.hr`
-  width : 1640px;
-  height : 0px;
-  border : 1px solid gray;
-  margin-top : -48px;
-`
+  width: ${(props) => props.width}px;
+  height: 0px;
+  border: 1px solid ${(props) => props.color};
+  margin-top: -48px;
+`;
+
+const LineBox = styled.div`
+  width: 1640px;
+  display: flex;
+`;
+
 export default MyPage;
