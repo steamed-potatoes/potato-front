2ba7b3bea280e2c392dd6490fd1aede3c8943918
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-05-20 20:21:21 +0900

    put과 그룹리스트 가져오기

--- src/page/myPage/TextBox.jsx
+++ src/page/myPage/TextBox.jsx
@@ -1,46 +1,109 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import * as actions from 'store/modules/user';
 import sendApi from 'apis/sendApi';
-// import { useDispatch } from 'react-redux';
 
+const Wrapper = styled.div`
+  height: 480px;
+  margin-left: 50px;
+`;
+const Field = styled.form`
+  width: 880px;
+  margin-left: 100px;
+`;
+
+const Input = styled.input`
+  font-size: 20px;
+  margin-bottom: 32px;
+  width: 400px;
+  border: none;
+  &:hover {
+    border-bottom: 2px solid #808080;
+  }
+  &:focus {
+    border-bottom: 2px solid #808080;
+    outline: none;
+  }
+`;
+const BoldInput = styled.input`
+  font-size: 24px;
+  margin-bottom: 32px;
+  width: 400px;
+  font-weight: bold;
+  border: none;
+  &:hover {
+    border-bottom: 2px solid #808080;
+  }
+  &:focus {
+    border-bottom: 2px solid #808080;
+    outline: none;
+  }
+`;
+const PTag = styled.p`
+  color: gray;
+`;
+const ModifyBtn = styled.button`
+  position: relative;
+  bottom: 70px;
+  left: 750px;
+  font-weight: bold;
+  border: none;
+  border-radius: 25px;
+  background-color: white;
+  width: 214px;
+  height: 42px;
+  box-shadow: 3px 3px 3px gray;
+  transition: transform 0.5s;
+  &:hover {
+    cursor: pointer;
+    font-weight: bold;
+    transform: scale(1.3);
+  }
+`;
 export const TextBox = () => {
-  // 서버에서 받아온 값을 나중에 초기값으로 설정
-  // const dispatch = useDispatch();
-
-  const [myGroup, setMyGroup] = useState('디자인-온');
-
-  // // 리덕스 상태가져옴
-  // const { email, name, profileUrl } = useSelector((state) => ({
-  //   email: state.user.email,
-  //   name: state.user.name,
-  //   profileUrl: state.user.profileUrl,
-  // }));
-
   const [name, setName] = useState('');
   const [major, setMajor] = useState('');
   const [classNumber, setClassNumber] = useState('');
   const [email, setEmail] = useState('');
+  const [myProfileUrl, setMyProfileUrl] = useState('');
+  const [groupNameList, setGroupNameList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // 객체 가져오기
-        const { data } = await sendApi.getMyProfile();
-
-        // APi에서 데이터 비구조화
-        const { email, name, major, classNumber } = data.data;
-
+        const { data: getMyProfile } = await sendApi.getMyProfile();
+        const {
+          email,
+          name,
+          major,
+          classNumber,
+          profileUrl,
+        } = getMyProfile.data;
         setName(name);
         setMajor(major);
         setClassNumber(classNumber);
         setEmail(email);
+        setMyProfileUrl(profileUrl);
+
+        const { data: groupListData } = await sendApi.getMyGroupList();
+        setGroupNameList(groupListData.data.map((data) => data.name));
       } catch (error) {
         alert(error.response.data.message);
       }
     };
     fetchData();
   }, []);
+  const handleClick = async () => {
+    try {
+      await sendApi.putMyProfile({
+        name,
+        myProfileUrl,
+        major,
+        classNumber,
+      });
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
 
   return (
     <Wrapper>
@@ -82,50 +145,15 @@ export const TextBox = () => {
           소속 동아리 :{' '}
           <BoldInput
             type="text"
-            value={myGroup}
-            onChange={(e) => setMyGroup(e.target.value)}
+            value={
+              groupNameList.length === 0
+                ? '아직 가입한 동아리가 없습니다.'
+                : groupNameList
+            }
           />
         </PTag>
       </Field>
+      <ModifyBtn onClick={handleClick}>수정</ModifyBtn>
     </Wrapper>
   );
 };
-const Wrapper = styled.div`
-  height: 480px;
-  margin-left: 50px;
-`;
-const Field = styled.form`
-  width: 880px;
-  margin-left: 100px;
-`;
-
-const Input = styled.input`
-  font-size: 20px;
-  margin-bottom: 32px;
-  width: 400px;
-  border: none;
-  &:hover {
-    border-bottom: 2px solid #808080;
-  }
-  &:focus {
-    border-bottom: 2px solid #808080;
-    outline: none;
-  }
-`;
-const BoldInput = styled.input`
-  font-size: 24px;
-  margin-bottom: 32px;
-  width: 400px;
-  font-weight: bold;
-  border: none;
-  &:hover {
-    border-bottom: 2px solid #808080;
-  }
-  &:focus {
-    border-bottom: 2px solid #808080;
-    outline: none;
-  }
-`;
-const PTag = styled.p`
-  color: gray;
-`;
