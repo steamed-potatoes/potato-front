e0903da32b087574dd5c7dd657b3615d01787bbd
minho0315 <dlalsgh369@naver.com>
2021-06-24 22:24:17 +0900

    오류 고침

--- src/constant/authkey.js
+++ src/constant/authkey.js
@@ -1,21 +1,13 @@
-// const {
-//   REACT_APP_CLIENT_ID,
-//   REACT_APP_REDIRECT_URI,
-//   REACT_APP_API_URI,
-// } = process.env;
+const {
+  REACT_APP_CLIENT_ID,
+  REACT_APP_REDIRECT_URI,
+  REACT_APP_API_URI,
+} = process.env;
 
-// export const AUTH_KEY = {
-//   apiUrl: REACT_APP_API_URI,
-//   google: {
-//     clientId: REACT_APP_CLIENT_ID,
-//     redirectUri: REACT_APP_REDIRECT_URI,
-//   },
-// };
 export const AUTH_KEY = {
-  apiUrl: 'https://api.pmarket.space',
+  apiUrl: REACT_APP_API_URI,
   google: {
-    clientId:
-      '255834671273-ltk6qjer04jlr3bunfdn60ki66fe752n.apps.googleusercontent.com',
-    redirectUri: 'http://localhost:3000/auth/google/callback',
+    clientId: REACT_APP_CLIENT_ID,
+    redirectUri: REACT_APP_REDIRECT_URI,
   },
 };
